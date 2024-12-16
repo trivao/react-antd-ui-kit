4b7e2969c9a8cd58f2ae06aa54dd774a225ec2b9
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-12-16 04:04:18 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import styles from 'rollup-plugin-styles';
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
