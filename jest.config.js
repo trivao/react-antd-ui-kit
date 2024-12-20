506bb935c22f50ab8f1b2a623fe5f3e0f43663cf
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-12-20 04:35:26 +0000

    feat(testing): added test mechanism

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+    coverageReporters: ["text", "lcov"],
+    coverageDirectory: "coverage",
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    },
+};
